a30d80068a339ab8f6235c78ea80fea51f616ab9
Santiago_Cocca <s.cocca.u@gmail.com>
2023-05-05 17:48:29 -0400

    se agrega el tachado del texto en las tareas, falta corregir que actuen juntos con el checkbox... tambien se cambian los colores del checkbox de acuerdo a su categoria.

--- app/src/main/java/com/example/androidmaster/PlannerApp/TaskViewHolder.kt
+++ app/src/main/java/com/example/androidmaster/PlannerApp/TaskViewHolder.kt
@@ -1,9 +1,11 @@
 package com.example.androidmaster.PlannerApp
 
+import android.content.res.ColorStateList
 import android.graphics.Paint
 import android.view.View
 import android.widget.CheckBox
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidmaster.R
 
@@ -17,9 +19,20 @@ class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         }else{
             tvTask.paintFlags=tvTask.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
         }
+
+        val color = when (task.category){
+            Taskcategories.Business -> R.color.todo_business_category
+            Taskcategories.Ocio -> R.color.todo_Ocio_category
+            Taskcategories.Other -> R.color.todo_other_category
+            Taskcategories.Personal -> R.color.todo_personal_category
+            Taskcategories.Sport -> R.color.todo_Sport_category
+        }
+
         tvTask.text = task.name
-
-
+        cbTask.isChecked=task.isSelected
+        cbTask.buttonTintList= ColorStateList.valueOf(
+            ContextCompat.getColor(cbTask.context, color)
+        )
     }
 
 }
